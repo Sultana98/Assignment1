994319bcf61b714b2ae28e6aaa198f55d9af0df4
Sultana98 <43569275+Sultana98@users.noreply.github.com>
2020-10-24 16:38:33 +0100

    Books Table Created

---
+++ database/factories/BooksFactory.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Books;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BooksFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Books::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->name,
+            'email' => $this->faker->email,
+            'phone' => $this->faker->phoneNumber,
+
+        ];
+    }
+}
